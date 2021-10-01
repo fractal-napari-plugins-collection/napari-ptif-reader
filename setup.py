6a5c0c4ae08b7cc324e9608b57b1ad9fe1aaa2a8
Dario Vischi <dario.vischi@fmi.ch>
2021-10-01 14:18:37 +0200

    Cleanup package

--- setup.py
+++ setup.py
@@ -1,38 +1,26 @@
 from setuptools import setup, find_packages
 
 
-# HACK: allows the execution of this script via pip, e.g. on a CI server.
-# pip tries to download private git repositories also they are already
-# installed on the system - which failes due missing credentials.
-private_install_requires = []
-try:
-    import pylibtiff
-except ModuleNotFoundError:
-    private_install_requires.append(
-        "pylibtiff @ git+https://github.com/fractal-analytics-platform/pylibtiff.git@v1.0.7"
-    )
-
-
 setup(
     name='fractal-napari-plugins-colletion:ptif-reader',
-    version='1.0.0',
+    version='1.1.0',
     author='Dario Vischi, Marco Franzon',
     author_email='dario.vischi@fmi.ch, marco.franzon@exact-lab.it',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    license='MIT',
+    license='BSD3',
     description=(
         'Plugin for reading multi-scale (pyramidal) TIFF files into Napari.'
     ),
     long_description=open('README.md').read(),
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=[
-        "napari[all] >= 0.3.8",
+        "napari[all] >= 0.4.7",
         "napari_plugin_engine >= 0.1.9",
-        "dask[complete] >= 2021.3.0",
-        "numpy >= 1.19.5",
+        "dask[complete] >= 2021.4.0",
+        "numpy >= 1.20.2",
         "imagecodecs >= 2020.5.30",
-        *private_install_requires
+        "pylibtiff @ git+https://github.com/fractal-napari-plugins-collection/pylibtiff.git@v1.0.7",
     ],
     entry_points={
         'napari.plugin': [
